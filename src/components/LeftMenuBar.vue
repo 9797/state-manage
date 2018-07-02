97bab50836242955b1111dcb23f41876333061d3
PUGE <100284685@qq.com>
2018-07-02 15:40:39 +0800

    基本完成

---
+++ src/components/LeftMenuBar.vue
@@ -0,0 +1,166 @@
+<template lang="pug">
+  //- 左侧二级菜单栏
+  .left-bar
+    .logo
+    .item-list(v-for="item in menuData", :key="item.name", :style="getItemHeight(item)", :class="{'open' : item.showmenu}")
+      .item-wrap(@click="item.showmenu = !item.showmenu")
+        <svg t="1530503401504" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4237" xmlns:xlink="http://www.w3.org/1999/xlink" width="16" height="16"><path d="M987.392 988.224H36.544c-20.096 0-36.544-14.336-36.544-32.128v-191.872c0-17.6 16.384-32 36.544-32h950.784c20.224 0 36.608 14.272 36.608 32v191.872c0.064 17.792-16.32 32.128-36.544 32.128zM146.304 785.6H60.992v74.624h85.312v-74.624z m499.84 0H560.832v149.248h85.312v-149.248z m158.336 0h-85.248v149.248h85.248v-149.248z m158.528 0h-85.312v149.248h85.312v-149.248zM987.392 654.656H36.544c-20.096 0-36.544-14.4-36.544-32.128V430.592c0-17.6 16.384-32 36.544-32h950.784c20.224 0 36.608 14.336 36.608 32v191.936c0.064 17.728-16.32 32.128-36.544 32.128zM146.304 451.904H60.992v74.688h85.312V451.904z m499.84 0H560.832v149.312h85.312V451.904z m158.336 0h-85.248v149.312h85.248V451.904z m158.528 0h-85.312v149.312h85.312V451.904zM987.392 320H36.544C16.448 320 0 305.664 0 287.936V96c0-17.6 16.384-32 36.544-32h950.784c20.288 0 36.672 14.336 36.672 32v191.936c0 17.728-16.384 32.064-36.608 32.064zM146.304 117.312H60.992V192h85.312V117.312z m499.84 0H560.832v149.312h85.312V117.312z m158.336 0h-85.248v149.312h85.248V117.312z m158.528 0h-85.312v149.312h85.312V117.312z" fill="" p-id="4238"></path></svg>
+        p.name {{item.name}}
+      .children(v-if="item.children && item.children.length > 0")
+        <svg class="drop-down" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5 10"><polygon points="0 5 5 10 5 0 0 5"/></svg>
+        router-link.child(v-for="(child, index) in item.children", :to="child.to", tag="div", :key="child.name")
+          .name {{child.name}}
+</template>
+
+<script>
+export default {
+  props: {
+    menuData: Array
+  },
+  mounted () {
+    for (let index in this.menuData) {
+      let item = this.menuData[index]
+      if (!item.children) continue
+      let children = item.children
+      let _this = this
+      children.forEach(child => {
+        let pathActive = _this.checkPath(child.to)
+        if (pathActive) {
+          item.showmenu = true
+          this.getItemHeight(item)
+        }
+      })
+    }
+  },
+  methods: {
+    // 根据子菜单数量计算出菜单高度
+    getItemHeight (menuItem) {
+      if (menuItem.showmenu) {
+        return { height: (menuItem.children.length + 1) * 60 + 'px' }
+      } else {
+        return { height: '60px' }
+      }
+    },
+    checkPath (to) {
+      let path = this.$route.path
+      let reg = new RegExp(`${to}`, 'g')
+      return reg.test(path)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .left-bar {
+    transition: width 0.5s;
+    background-color: white;
+    width: 200px;
+    position: relative;
+    overflow: hidden;
+    box-shadow: 7px 0px 10px #edf3ff;
+    .open .drop-down {
+      transform: rotate(90deg);
+    }
+    .item-list {
+      width: 100%;
+      height: 60px;
+      font-size: 1.1em;
+      overflow: hidden;
+      color: #666;
+      line-height: 60px;
+      align-items: center;
+      user-select: none;
+      cursor: pointer;
+      position: relative;
+      text-decoration: none;
+      transition: ALL 0.2s ease-out;
+      letter-spacing: 4px;
+      .router-link-active {
+        border-left: 5px solid #59ADDA;
+        background: rgba(235,235,235,0.50);
+        .icon {
+          color:#59ADDA;
+        }
+      }
+      .children {
+        height: auto;
+        overflow: hidden;
+        background-color: #f5f9ff;
+      }
+      .child {
+        transition: all .3s;
+        display: flex;
+        align-items: center;
+        width: 150px;
+        height: 60px;
+        padding-left: 50px;
+        &:hover {
+          background: rgba(89, 173, 218, 0.07);
+          border-left: 5px solid #59ADDA;
+          width: 145px;
+          .name {
+            color: #59ADDA;
+          }
+        }
+        .icon {
+          width: 30px;
+          text-align: center;
+          color: #686868;
+          pointer-events: none;
+        }
+        .name {
+          color: #686868;
+          height: 60px;
+          line-height: 60px;
+          pointer-events: none;
+        }
+      }
+    }
+  }
+  .item-wrap {
+    display: flex;
+    align-items: center;
+    width: auto;
+    height: 60px;
+    font-size: 1.1em;
+    position: relative;
+    border-left: 5px solid transparent;
+    .icon {
+      width: 60px;
+      height: 20px;
+      fill: #686868;
+      pointer-events: none;
+    }
+    .name {
+      overflow: hidden;
+      color: #686868;
+      pointer-events: none;
+    }
+  }
+  .logo {
+    margin: 0 20px;
+    padding: 20px 0;
+    height: 50px;
+    border-bottom: 1px solid #dddddd;
+  }
+  .drop-down {
+    position: absolute;
+    width: 6px;
+    right: 25px;
+    top: 24px;
+    fill: #686868;
+  }
+  @media screen and (max-width: 1920px) {
+    .left-bar {
+      width: 160px;
+    }
+    .item-wrap {
+      .icon {
+        width: 40px;
+      }
+      .name {
+        letter-spacing: normal;
+      }
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,270 @@
-<template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  .home
+    LeftMenuBar.left(:menuData="menuData")
+    .right-panel
+      .title XX系统于2018-10-12故障，已下发短信提醒.
+      .tool-bar
+        .sort 故障系统优先
+        CheckBox.check(:size="18")
+        .clear-message 全部标记为已读
+      .state-panel
+        .state-item(v-for="item in mock", :class="{warn: item.state === 1, error: item.state === 2}")
+          .service
+          .name {{item.name}}
+          img.state-item-icon(v-if="item.state === 0", src="../assets/right.svg")
+          img.state-item-icon(v-if="item.state === 1", src="../assets/warn.svg")
+          img.state-item-icon(v-if="item.state === 2", src="../assets/error.svg")
+          .mark(v-if="item.message > 0") {{item.message}}
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import CheckBox from 'check-puge'
+import LeftMenuBar from '@/components/LeftMenuBar.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    CheckBox,
+    LeftMenuBar
+  },
+  data () {
+    return {
+      menuData: [
+        {to: '/control', icon: '&#xe626;', name: '大屏管理', routerActive: false},
+        {to: '/template', icon: '&#xe66a;', name: '模板管理', routerActive: false},
+        {
+          to: '/dataBase',
+          icon: '&#xe630;',
+          name: '数据管理',
+          showmenu: false,
+          children: [
+            {
+              to: '/dataBase/file',
+              name: '文件管理'
+            },
+            {
+              to: '/dataBase/data',
+              name: '数据管理'
+            }
+          ]
+        },
+        {
+          to: '/sms',
+          icon: '&#xe64d;',
+          name: '短信管理',
+          showmenu: false,
+          children: [
+            {
+              to: '/sms/template',
+              name: '短信模板'
+            },
+            {
+              to: '/sms/systemList',
+              name: '系统管理'
+            },
+            {
+              to: '/sms/sendRecord',
+              name: '发送记录'
+            }
+          ]
+        }
+      ],
+      mock: [
+        {name: '系统名称', state: 1, message: 0},
+        {name: '系统名称', state: 1, message: 0},
+        {name: '系统名称', state: 2, message: 0},
+        {name: '系统名称', state: 2, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 3},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0},
+        {name: '系统名称', state: 0, message: 0}
+      ]
+    }
   }
 }
 </script>
+
+<style lang="less" scoped>
+  .home {
+    height: 100%;
+    width: 100%;
+    display: flex;
+  }
+  .left {
+    height: 100%;
+  }
+  .right-panel {
+    height: 100%;
+    width: calc(~"100% - 200px");
+    .tool-bar {
+      height: 60px;
+      margin: 0 70px;
+      line-height: 60px;
+      font-size: 1.4rem;
+      color: #4a4a4a;
+      display: flex;
+    }
+    .check {
+      margin: 19px;
+    }
+    .clear-message {
+      color: #6db7df;
+      margin: 0 20px;
+      cursor: pointer;
+    }
+  }
+  .title {
+    height: 89px;
+    margin: 0 40px;
+    margin-top: 20px;
+    line-height: 90px;
+    color: #6c6c6c;
+    font-size: 1.4rem;
+    width: calc(100% - 380px);
+    border-bottom: 1px solid #dddddd;
+  }
+  .state-panel {
+    margin: 0 40px;
+    padding: 15px;
+    overflow-x: hidden;
+    overflow-y: auto;
+    height: calc(100% - 200px);
+  }
+  .state-item {
+    height: 150px;
+    width: 150px;
+    float: left;
+    margin: 15px;
+    position: relative;
+    border-radius: 10px;
+    background-color: #d3f0d9;
+    .service {
+      height: 110px;
+      background-size: 100px;
+      background-position: 25px;
+      background-repeat: no-repeat;
+      background-image: url(../assets/computer.svg)
+    }
+    .name {
+      height: 30px;
+      line-height: 30px;
+      font-size: 1.2rem;
+      text-align: center;
+    }
+    .state-item-icon {
+      width: 25px;
+      position: absolute;
+      left: 63px;
+      top: 37px;
+    }
+    .mark {
+      position: absolute;
+      background-color: red;
+      width: 50px;
+      line-height: 25px;
+      color: white;
+      text-align: center;
+      border-radius: 15px;
+      right: -15px;
+      top: -15px;
+      font-size: 1.2rem;
+    }
+  }
+  .warn {
+    background-color: #ffce0e;
+  }
+  .error {
+    background-color: #ff7f7f;
+  }
+</style>
