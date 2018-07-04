de789279dba4243e323be4c2ff00f9b0832e28ff
yangyufeng <1539558157@qq.com>
2018-07-04 11:42:35 +0800

    系统详情的鼠标悬浮

--- src/views/SysDetail.vue
+++ src/views/SysDetail.vue
@@ -19,6 +19,7 @@ require('echarts/lib/chart/bar')
 require('echarts/lib/chart/line')
 // 引入提示框和图例组件
 require('echarts/lib/component/tooltip')
+require('echarts/lib/component/axisPointer')
 require('echarts/lib/component/legend')
 require('echarts/lib/component/title')
 require('echarts/lib/component/toolbox')
@@ -32,7 +33,6 @@ export default {
     }
   },
   mounted () {
-    console.log(this.chartOption)
     this.chart = echarts.init(this.$refs.chart)
     this.chart.setOption(this.chartOption)
     let _this = this
@@ -43,47 +43,40 @@ export default {
     })
   },
   created () {
-    // 增加toolbox的自定义定点击事件
-    this.chartOption.toolbox.feature.myTool1.onclick = this.changeBar
-    this.chartOption.toolbox.feature.myTool2.onclick = this.changeLine
+    // this.chartOption.tooltip.triggerOn = {
+    //   'mousemove|click': (params) => {
+    //     console.log(params)
+    //   }
+    // }
   },
   methods: {
-    // 变成柱状图
-    changeBar () {
-      let option = this.chartOption
-      option.series[0].type = 'bar'
-      option.title.show = false
-      option.xAxis.boundaryGap = true
-      this.chart.setOption(this.chartOption)
-    },
-    // 变成折线图
-    changeLine () {
-      let option = this.chartOption
-      option.series[0].type = 'line'
-      option.xAxis.boundaryGap = false
-      option.title.show = true
-      this.chart.setOption(this.chartOption)
-    },
     // 数据缩放
     dataZoom (type) {
       let option = this.chartOption
       let dataZoom = option.dataZoom[0]
+      let dates = option.xAxis.data
       // 减
       if (type === 'reduce') {
         let newStartValue = dataZoom.startValue - step
-        let newEndValue = dataZoom.endValue - step
-        if (newStartValue < 0) return
+        if (newStartValue < dates[0]) return
         dataZoom.startValue = newStartValue
-        if (newEndValue < 10) return
-        dataZoom.endValue = newEndValue
-      } else {
-        // debugger
-        // 加
-        dataZoom.startValue += step
         dataZoom.endValue += step
+      } else if (type === 'add') {
+        // 加
+        console.log('x轴最大值', dates[dates.length-1])
+        let newEndValue = dataZoom.endValue + step
+        console.log('新的dataZoom.endValue', newEndValue)
+        if (newEndValue > dates[dates.length-1]) return
+        dataZoom.endValue = newEndValue
+        dataZoom.startValue += step
       }
       console.log('缩放后的dataZoom', this.chartOption.dataZoom)
       this.chart.setOption(this.chartOption)
+    },
+    showTitleTip (params) {
+      console.log(params)
+      let option = this.chartOption
+      option.title.show = true
     }
   },
   beforeDestroy () {
@@ -129,12 +122,12 @@ export default {
       .prev {
         background: url(./../assets/left-jiantou.svg) no-repeat left center;
         background-size: cover;
-        left: 50px;
+        left: 15px;
       }
       .next {
         background: url(./../assets/right-jiantou.svg) no-repeat left center;
         background-size: cover;
-        right: 120px;
+        right: 85px;
       }
     }
   }
